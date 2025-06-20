d86cdef5e1e291ad9aaf8d6fe3e0a33daf3ad570
arjun-saseendran <arjunsaseendran@icloud.com>
2025-06-20 12:47:04 +0530

    add home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import { PostBar } from "../../components/PostBar/PostBar";
+import { PostCard } from "../../components/PostCard/PostCard";
+import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
+import { Header } from "../../components/Header/Header";
+import { JobUpdates } from "../../components/JobUpdates/JobUpdates";
+import { Analytics } from "../../components/Analytics/Analytics";
+import { Shortcuts } from "../../components/Shortcuts/Shortcuts";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="w-full">
+        <Header />
+      </div>
+      <div className="flex gap-2 lg:gap-1 xl:gap-10 mt-2 xl:mx-48 lg:mx-auto md:pt-14    m-1">
+        <div className="hidden md:flex flex-col gap-2">
+          <ProfileCard />
+          <Analytics />
+          <Shortcuts />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            <PostBar />
+          </div>
+          <div>
+            <PostCard />
+          </div>
+        </div>
+
+        <div className="hidden lg:block">
+          <JobUpdates />
+        </div>
+      </div>
+    </>
+  );
+};
