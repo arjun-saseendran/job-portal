6e198ebeddb54ec80ed6c2fe040bd627b49f7cc1
arjun-saseendran <arjunsaseendran@icloud.com>
2025-07-01 06:56:45 +0530

    add searchSlice

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { App } from "./App.jsx";
+import { store } from "./redux/store.js";
 import { Home } from "./pages/Home/Home.jsx";
 import { Jobs } from "./pages/jobs/Jobs.jsx";
 import {Root} from "./routes/Root";
@@ -32,6 +32,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} store={store} />
   </StrictMode>,
 );
---
+++ src/redux/features/searchSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState: "",
+  reducers: {
+    saveSearchQuery: (_, action) => action.payload,
+  },
+});
+
+export const { saveSearchQuery } = searchSlice.actions;
+export default searchSlice.reducer;
